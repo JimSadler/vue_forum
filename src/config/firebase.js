3489266188509d58c8429c7924bc6c3614043139
JimSadler <jim@jsdevelopment.io>
2021-12-13 10:51:17 -0500

    initial firestore initialization and firebase database add

---
+++ src/config/firebase.js
@@ -0,0 +1,9 @@
+// Your web app's Firebase configuration
+module.exports = {
+  apiKey: 'AIzaSyAXdAxCd39mTBmGPuWeSm39wQ7Jkai4vCU',
+  authDomain: 'vueforum-97ff6.firebaseapp.com',
+  projectId: 'vueforum-97ff6',
+  storageBucket: 'vueforum-97ff6.appspot.com',
+  messagingSenderId: '943092394845',
+  appId: '1:943092394845:web:41931cd01ca03c79a6d6b4'
+}
