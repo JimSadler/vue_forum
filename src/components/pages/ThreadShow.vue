a054a98687d3ba43fbc75fc34536eb603f8f0796
JimSadler <jim@jsdevelopment.io>
2021-12-23 15:22:02 -0500

    migrated vuex to use cloud firestore database

--- src/components/pages/ThreadShow.vue
+++ src/components/pages/ThreadShow.vue
@@ -10,7 +10,7 @@
       >
     </h1>
     <p>
-      By <a href="#" class="link-unstyled">{{ thread.author.name }}</a
+      By <a href="#" class="link-unstyled">{{ thread.author?.name }}</a
       >, <AppDate :timestamp="thread.publishedAt" />.
       <span
         style="float: right; margin-top: 2px;"
@@ -27,6 +27,7 @@
 <script>
 import PostList from '@/components/PostList'
 import PostEditor from '@/components/PostEditor'
+import firebase from 'firebase/compat/app'
 // import { findById } from '@/helpers'
 export default {
   components: {
@@ -63,6 +64,48 @@ export default {
       }
       this.$store.dispatch('createPost', post)
     }
+  },
+  created() {
+    // fetch the thread
+    firebase
+      .firestore()
+      .collection('threads')
+      .doc(this.id)
+      .onSnapshot((doc) => {
+        const thread = { ...doc.data(), id: doc.id }
+        this.$store.commit('setThread', { thread })
+
+        // fetch the user
+        firebase
+          .firestore()
+          .collection('users')
+          .doc(thread.userId)
+          .onSnapshot((doc) => {
+            const user = { ...doc.data(), id: doc.id }
+            this.$store.commit('setUser', { user })
+          })
+
+        // fetch the posts
+        thread.posts.forEach((postId) => {
+          firebase
+            .firestore()
+            .collection('posts')
+            .doc(postId)
+            .onSnapshot((doc) => {
+              const post = { ...doc.data(), id: doc.id }
+              this.$store.commit('setPost', { post })
+              // fetch the user for each post
+              firebase
+                .firestore()
+                .collection('users')
+                .doc(post.userId)
+                .onSnapshot((doc) => {
+                  const user = { ...doc.data(), id: doc.id }
+                  this.$store.commit('setUser', { user })
+                })
+            })
+        })
+      })
   }
 }
 </script>
--- src/store/index.js
+++ src/store/index.js
@@ -1,10 +1,13 @@
 import { createStore } from 'vuex'
-import sourceData from '@/data'
 import { findById, upsert } from '@/helpers'
 
 export default createStore({
   state: {
-    ...sourceData,
+    categories: [],
+    forums: [],
+    threads: [],
+    posts: [],
+    users: [],
     authId: 'VXjpr2WHa8Ux4Bnggym8QFLdv5C3'
   },
   getters: {
@@ -60,9 +63,8 @@ export default createStore({
       parent: 'threads',
       child: 'posts'
     }),
-    setUser(state, { user, userId }) {
-      const userIndex = state.users.findIndex((user) => user.id === userId)
-      state.users[userIndex] = user
+    setUser(state, { user }) {
+      upsert(state.users, user)
     },
     setThread(state, { thread }) {
       upsert(state.threads, thread)
