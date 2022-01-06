370647610926d48b8e415cca56cb8d5fff2fef68
JimSadler <jim@jsdevelopment.io>
2022-01-06 11:07:50 -0500

    wip troubleshooting

---
+++ src/config/firebase.js
@@ -0,0 +1,8 @@
+module.exports = {
+  apiKey: 'AIzaSyAvgO9DJirrTCePJpQGG3TmBjwCqIDtD7o',
+  authDomain: 'vueforum-98109.firebaseapp.com',
+  projectId: 'vueforum-98109',
+  storageBucket: 'vueforum-98109.appspot.com',
+  messagingSenderId: '691482465709',
+  appId: '1:691482465709:web:82573faee75f500fc07d24'
+}
